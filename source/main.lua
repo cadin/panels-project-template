0621c7a71cf4952fa583edeb92b81ad6bc78f5ba
Cadin Batrack <cabatrac@microsoft.com>
2021-10-13 16:44:20 -0700

    add note about intializing submodule

--- source/main.lua
+++ source/main.lua
@@ -1,10 +1,16 @@
+-- IMPORT:
+-- Panels is included as a submodule in this repo
+-- if you don't see any files in libraries/panels  
+-- you may need to initialize the submodule
 import "libraries/panels/Panels"
 
+-- CREDITS:
 -- edit the table inside `credits.lua`
 -- to create your game credits
 import "gameCredits.lua"
 Panels.credits = gameCredits
 
+-- COMIC DATA:
 -- add data to the table in this file to create your comic
 import "myComicData.lua"
 comicData = myComicData
@@ -20,6 +26,6 @@ import "examples/1-simple-comic.lua"
 -- import "examples/4-custom-functions.lua"
 
 
-
+-- START:
 -- send the data table of your comic (or an example above) to the `start()` command
 Panels.start(comicData)
