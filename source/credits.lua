5921b175076dcd2e461fabb3c20fc85cc0a12bd1
Cadin Batrack <cabatrac@microsoft.com>
2021-10-12 17:44:53 -0700

    initial commit

---
+++ source/credits.lua
@@ -0,0 +1,17 @@
+-- the Credits screen is accessed from 
+-- the Playdate system menu
+
+credits = {
+
+    -- settings for your credits screen
+    alignment = Panels.TextAlignment.CENTER,
+	autoScroll = false,
+
+    -- the actual credits for your comic
+    -- add as many as you like, the page will scroll
+    -- you can add images here too
+    lines = {
+		{ text = "A Very Nice Comic"},
+		{ text = "created by *Me*" },
+    }
+}
---
+++ source/main.lua
@@ -0,0 +1,9 @@
+import "libraries/panels/Panels"
+
+-- edit the table inside `credits.lua`
+-- to create your game credits
+import "credits.lua"
+Panels.credits = credits
+
+
+Panels.start(comicData)
